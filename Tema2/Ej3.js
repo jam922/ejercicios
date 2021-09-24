67fb14050a0c763a7271bdc395b073e4ceb1ae5d
jam922 <jam.castro36@gmail.com>
2021-09-24 17:56:20 +0200

    Ej3

---
+++ Tema2/Ej3.js
@@ -0,0 +1,20 @@
+var n1 = prompt("Intrduce un número entero");
+n1 = parseInt(n1);
+
+var n2 = prompt("Intrduce un segundo número entero");
+n2 = parseInt(n2);
+
+var n3 = prompt("Intrduce un tercer número entero");
+n3 = parseInt(n3);
+
+if (n1 > 10){
+	alert("El numero "+n1+" es mayor que 10");
+}  
+
+if (n2>10){
+	alert("El numero "+n2+" es mayor que 10");
+} 
+
+if(n3>10){ 
+	alert("El numero "+n3+" es mayor que 10");
+}
