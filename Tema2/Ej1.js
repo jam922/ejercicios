538f4e69cc2aabb940e6f12c74fcf814f7c3dd79
jam922 <jam.castro36@gmail.com>
2021-09-24 17:30:35 +0200

    Mi primera subida

---
+++ Tema2/Ej1.js
@@ -0,0 +1,17 @@
+
+var n1 = prompt("Intrduce un número entero");
+n1 = parseInt(n1);
+
+var n2 = prompt("Intrduce un segundo número entero");
+n2 = parseInt(n2);
+
+var n3 = prompt("Intrduce un tercer número entero");
+n3 = parseInt(n3);
+
+console.log(n1,n2,n3);
+
+var nom = prompt("Intrduce tu nombre");
+
+var ape = prompt("Intrduce tus apellidos");
+
+console.log("Mi nombre y apellidos son: "+nom,ape);
---
+++ Tema2/Ej2.js
@@ -0,0 +1,22 @@
+var n1 = prompt("Intrduce un número entero");
+n1 = parseInt(n1);
+
+var n2 = prompt("Intrduce un segundo número entero");
+n2 = parseInt(n2);
+
+var n3 = prompt("Intrduce un tercer número entero");
+n3 = parseInt(n3);
+
+alert("Los numeros obtenidos son: "+n1+" ,"+n2+" y  "+n3);
+
+alert("La suma de los tres numeros son: "+(n1+n2+n3));
+
+alert("La multiplicacion de los tres numeros es: "+(n1*n2*n3));
+
+alert("La division de los tres numeros es: "+(n1/n3));
+
+var nom = prompt("Intrduce tu nombre");
+
+var ape = prompt("Intrduce tus apellidos");
+
+alert("Mi nombre y apellidos son: "+nom+" "+ape);
