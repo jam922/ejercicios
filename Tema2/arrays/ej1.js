92199120bc39f2a512558dc27e31da9191e4096a
jam922 <91147911+jam922@users.noreply.github.com>
2021-10-29 19:22:17 +0200

    Add files via upload

---
+++ Tema2/arrays/ej1.js
@@ -0,0 +1,30 @@
+
+	
+	let nombres = [];
+
+
+			for(i=0;i<3;i++){
+
+			let nombre = prompt("Introduzca el socio "+(i+1)+": ");
+			nombre=String(nombre);
+			nombres[i]=nombre;
+
+			}
+			var vs=null;
+
+			function abrir(){
+				vs = window.open("resul.html");
+				
+			}
+	/*
+	alert(`Los nombres de los socios son:\n ${nombres}`);	
+	alert(`Hay ${nombres.length} socios`);
+	alert(`Ordenación alfabeticamente:\n ${nombres.sort()}`);
+	alert(`Ordenación al reves:\n ${nombres.reverse()}`);
+		*/
+	
+
+	
+
+
+	
